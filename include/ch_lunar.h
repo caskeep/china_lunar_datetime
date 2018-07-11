9076f727c15ee52401b92b6ea350db2867c625cd
Mithwindy <khuangustc@gmail.com>
2018-07-12 00:16:49 +0800

    add basic function and test;

--- include/ch_lunar.h
+++ include/ch_lunar.h
@@ -9,4 +9,40 @@
 #include <chrono>
 
 
+class ChLunarDate
+{
+public:
+    ChLunarDate() = default;
+    ~ChLunarDate() = default;
+
+public:
+    int
+    GetYear() {return second_.year(); }
+
+    int
+    GetMonthOfYear() {return second_.month(); }
+
+    int
+    GetDayOfMonth() { return second_.day(); }
+
+    int
+    GetDayOfWeek() {
+        cctz::civil_day day(second_);
+        static_cast<int>(cctz::get_weekday(day)) + 1;
+    }
+
+    int
+    GetHourOfDay() { return second_.hour(); }
+
+    int
+    GetMinuteOfHour() { return second_.minute(); }
+
+    int
+    GetSecondOfMinute() {return second_.second(); }
+
+public: // TODO
+    cctz::civil_second second_;
+};
+
+
 #endif //CHINA_LUNAR_DATETIME_CH_LUNAR_H
--- test/main.cpp
+++ test/main.cpp
@@ -7,20 +7,45 @@
 
 int main(int argc, char *argv[]) {
     {
+        std::cout << "## Basic Test." << std::endl;
         const std::string civil_string = "2015-09-22 09:35:00";
 
         std::chrono::system_clock::time_point tp;
 
-        const auto        now = std::chrono::system_clock::now();
-        const std::string s   = now > tp ? "running long!" : "on time!";
+        const auto        now         = std::chrono::system_clock::now();
+        const std::string s           = now > tp ? "running long!" : "on time!";
 
         std::cout << "Talk " << s << "\n";
 
         cctz::civil_day civilDay(2018, 7, 9);
 //        std::cout << civilDay << std::endl;
-        const auto dayOfWeek = static_cast<int>(cctz::get_weekday(civilDay));
+        const auto      dayOfWeek     = static_cast<int>(cctz::get_weekday(civilDay));
+
         std::cout << "civil day year: " << civilDay.year() << " month: " << civilDay.month() << " day of month: "
                   << civilDay.day() << " day of week: " << dayOfWeek << std::endl;
+
+        cctz::civil_second civilSecond(2018, 7, 9, 10, 11, 25);
+        const auto         dayOfWeek2 = static_cast<int>(cctz::get_weekday(civilDay));
+
+        std::cout << "civil second year: " << civilSecond.year() << " month: " << civilSecond.month()
+                  << " day of month: "
+                  << civilSecond.day() << " day of week: " << dayOfWeek2 << " hour of day: " << civilSecond.hour()
+                  << std::endl;
+    }
+
+    {
+        std::cout << "## Class ChLunarDate Test." << std::endl;
+        cctz::civil_second civilSecond(2018, 7, 9, 10, 11, 25);
+        ChLunarDate        chLunarDate;
+        chLunarDate.second_ = civilSecond;
+        std::cout << "year:             " << chLunarDate.GetYear() << std::endl
+                  << "month:            " << chLunarDate.GetMonthOfYear() << std::endl
+                  << "day of month:     " << chLunarDate.GetDayOfMonth() << std::endl
+                  << "day of week:      " << chLunarDate.GetDayOfWeek() << std::endl
+                  << "hour of day:      " << civilSecond.hour() << std::endl
+                  << "minute of hour:   " << civilSecond.minute() << std::endl
+                  << "second of minute: " << civilSecond.second() << std::endl
+                  << std::endl;
     }
     return 0;
 }
