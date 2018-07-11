048cb6f40a7521da19fcde3adee891445a1e3b57
Mithwindy <khuangustc@gmail.com>
2018-07-11 10:31:47 +0800

    refine;

--- test/main.cpp
+++ test/main.cpp
@@ -16,10 +16,10 @@ int main(int argc, char *argv[]) {
 
         std::cout << "Talk " << s << "\n";
 
-        cctz::civil_day civilDay(2018, 7, 10);
+        cctz::civil_day civilDay(2018, 7, 9);
 //        std::cout << civilDay << std::endl;
-        const int dayOfWeek = static_cast<int>(cctz::get_weekday(civilDay));
-        std::cout << "civil day year: " << civilDay.year() << " month: " << civilDay.month() << " day of month:"
+        const auto dayOfWeek = static_cast<int>(cctz::get_weekday(civilDay));
+        std::cout << "civil day year: " << civilDay.year() << " month: " << civilDay.month() << " day of month: "
                   << civilDay.day() << " day of week: " << dayOfWeek << std::endl;
     }
     return 0;
